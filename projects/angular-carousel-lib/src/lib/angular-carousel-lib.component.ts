0101b5079db83473f315c04d303d2625170d4a4a
Josh Resnick <>
2019-03-17 11:56:56 -0400

    component is building and working

--- projects/angular-carousel-lib/src/lib/angular-carousel-lib.component.ts
+++ projects/angular-carousel-lib/src/lib/angular-carousel-lib.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, ViewChildren, QueryList, ElementRef, ViewChild } from '@angular/core';
+import { TimelineLite } from 'gsap'
 @Component({
   selector: 'enl-angular-carousel-lib',
-  template: `
-    <p>
-      angular-carousel-lib works!
-    </p>
-  `,
+  templateUrl: './angular-carousel-lib.template.html',
   styles: []
 })
+
 export class AngularCarouselLibComponent implements OnInit {
 
-  constructor() { }
+  @Input() images: any[];
 
+  @ViewChild('carouselContainer') carouselContainer: ElementRef;
+  @ViewChild('arrowLeft') arrowLeft: ElementRef;
+  @ViewChild('arrowRight') arrowRight: ElementRef;
+  @ViewChildren('item') items: QueryList<ElementRef>;
+  // @Input() masterName: string;
+
+  constructor() { }
+  
   ngOnInit() {
+    console.log(this.images)
   }
 
+
+  rotateRight(): void {
+    console.log('Rotate Right')
+  }
+
+  rotateLeft(): void {
+    console.log('Rotate Left')
+  }
+
+  chooseSlideDir(slideNum): void {
+    console.log('chooseSlideDir')
+  }
 }
--- projects/angular-carousel-lib/src/lib/angular-carousel-lib.module.ts
+++ projects/angular-carousel-lib/src/lib/angular-carousel-lib.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AngularCarouselLibComponent } from './angular-carousel-lib.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AngularCarouselLibComponent],
   imports: [
+    CommonModule,
+    BrowserModule
   ],
   exports: [AngularCarouselLibComponent]
 })
